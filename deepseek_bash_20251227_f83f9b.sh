874dc7c21cb8ab05f2839ea1198f500f5bcb5754
kinguelectricaltz-hub <kinguelectricaltz@gmail.com>
2025-12-27 18:13:34 +0300

    Delete deepseek_bash_20251227_f83f9b.sh

--- deepseek_bash_20251227_f83f9b.sh
+++
@@ -1,6 +0,0 @@
-# Create folder and add files
-mkdir -p kinguelectrical-website/{assets/{css,js,images/{certificates,projects,clients}},uploads,.github/workflows}
-
-# Add all files to their locations
-# Then create ZIP:
-zip -r kinguelectrical-website.zip kinguelectrical-website/
