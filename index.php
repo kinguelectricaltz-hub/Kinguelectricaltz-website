c96847ccc8fb7aed55859e70397f787b6a629d43
kinguelectricaltz-hub <kinguelectricaltz@gmail.com>
2025-12-27 18:20:46 +0300

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-<main>
-  <h1>Welcome to Kingu Electrical Company Limited</h1>
-  <p>Quality Electrical Services with a Professional Experience</p>
-</main>
-<?php get_footer(); ?>
