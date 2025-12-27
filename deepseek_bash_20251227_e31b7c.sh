4ef58ec6d32ef4bfc012861a17889f7a590d2437
kinguelectricaltz-hub <kinguelectricaltz@gmail.com>
2025-12-27 18:12:01 +0300

    Delete deepseek_bash_20251227_e31b7c.sh

--- deepseek_bash_20251227_e31b7c.sh
+++
@@ -1,7 +0,0 @@
-# Create main folder
-mkdir kinguelectrical-website
-cd kinguelectrical-website
-
-# Create folder structure
-mkdir -p assets/{css,js,images/{certificates,projects,clients}}
-mkdir -p uploads .github/workflows
